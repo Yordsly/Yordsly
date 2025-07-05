415d2779ebab97ddb5db56aea6baa39847c69e17
Yordsly <hileman624@gmail.com>
2025-07-05 19:50:09 -0400

    Added a Header and Light/Dark Mode

--- yordsly/src/app/app.ts
+++ yordsly/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header.component';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
 export class App {
   protected title = 'yordsly';
+
 }
