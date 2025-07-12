be6956aa33f78b2b2080ab10300d65bc0e576e66
Yordsly <hileman624@gmail.com>
2025-07-11 22:32:07 -0400

    Updated Login Information

--- yordsly/src/app/components/landing-page.component/landing-page.component.ts
+++ yordsly/src/app/components/landing-page.component/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -15,5 +16,16 @@ import { MatMenuModule } from '@angular/material/menu';
   standalone: true
 })
 export class LandingPageComponent {
+  readonly userService = inject(UserService);
+  isLoggedIn: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    // Subscribe to user service to check login status
+    this.userService.currentUser$.subscribe(user => {
+      this.isLoggedIn = !!user; // Set isLoggedIn based on user presence
+    });
+  }
 
 }
